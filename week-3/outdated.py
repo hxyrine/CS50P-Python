d80cf7851900a5b7ce21865f2e4e66f233253636
Hewlett Xyrine Madrid <hbmadrid1@up.edu.ph>
2025-12-10 19:13:56 +0800

    Revised according to failed test cases

--- week-3/outdated.py
+++ week-3/outdated.py
@@ -1,6 +1,3 @@
-# Input M/D/Y format
-# Convert to Y/M/D format
-
 months = [
     "January",
     "February",
@@ -17,15 +14,22 @@ months = [
 ]
 while True:
     try:
-        date = input()
-        date = date.replace("/", " ").split()
-        m, d, y = [i.strip("/,") for i in date]
-        if m.isdigit() and (int(m) <= len(months) - 1) and 0 < int(d) <= 31:
-            print(f"{y}-{int(m):02}-{d:02}")
-            break
-        elif m.isalpha() and 0 < int(d) <= 31:
-            print(f"{y}-{months.index(m) + 1:02}-{d}")
-            break
-        continue
+        date = input("Date: ").strip()
+        if "/" in date:
+            m, d, y = map(int, date.split("/"))  # If m is str, will produce ValueError; reprompt
+            if (0 < m <= 12) and (0 < d <= 31):  # Ensures m is within 12 and d is within 31
+                print(f"{y}-{m:02}-{d:02}")
+                break
+        elif "," in date:
+            date = date.split(" ")
+            m, d, y = [i.strip(",") for i in date]
+            d, y = map(int, (d, y))
+            if not m.isalpha():
+                break
+            if (0 < d <= 31):
+                print(f"{y}-{months.index(m) + 1:02}-{d:02}")
+                break
     except ValueError:
         continue
+
+    # I feel like it's a bit crudely made but I tried 🥲
